263951b343489dc04d567e3c9541d2e932cdda4a
garmoncheg <garmon1@gmail.com>
2017-07-25 13:54:09 +0300

    serve data locally

--- TestApp/app.py
+++ TestApp/app.py
@@ -1,18 +1,16 @@
 import dash_components_table_own as dto
 import dash
 import dash_html_components as html
+import os
 
 import pandas as pd
 
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/'
-    'c78bf172206ce24f77d6363a2d754b59/raw/'
-    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
-    'usa-agricultural-exports-2011.csv')
+# Loading a file
+data_path = os.path.join(os.getcwd(), os.path.join('..', 'data'))
+data_file = os.path.join(data_path, 'usa-agricultural-exports-2011.csv')
+df = pd.read_csv(data_file)
 
-app = dash.Dash('')
-
-
+# Still making a table inside a python code.
 def generate_table(dataframe, max_rows=10):
     return [html.Tr([html.Th(col) for col in dataframe.columns])] + \
         [html.Tr([
@@ -21,7 +19,6 @@ def generate_table(dataframe, max_rows=10):
 
 
 app = dash.Dash()
-
 app.layout = html.Div(children=[
     dto.TableOwn(generate_table(df), label="US Agriculture Exports (2011)", id='my-table', wrapperClass='test-class')
 ])
