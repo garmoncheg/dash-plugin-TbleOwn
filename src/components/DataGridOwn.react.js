0765af6781867a748fd314fdc83070d6b9f0befc
garmoncheg <garmon1@gmail.com>
2017-07-26 16:31:27 +0300

    fix unique keys problem in table rendering.

--- src/components/DataGridOwn.react.js
+++ src/components/DataGridOwn.react.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 
 export default class DataGridOwn extends Component {
     render() {
+        let uuid = 0;
         const props = this.props;
         const label = props.label;
         const csv_data = props.csv_data;
 
         // Demo data
-        const CONTACTS = csv_data || [
+        const DATA = csv_data || [
             ['', 'id', 'name', 'phoneNumber', 'image'],
             ['0','Darth Vader', '+250966666666', 'img/darth.gif'],
             ['1','Princess Leia', '+250966344466','img/leia.gif'],
@@ -35,7 +36,11 @@ export default class DataGridOwn extends Component {
                 return <tr>
                     {
                         this.props.td.map(function(el) {
-                            return <TD key={el} td={el} />
+                            uuid = uuid + 1;
+                            return <TD
+                                key={uuid}
+                                td={el}
+                            />
                         })
                     }
                 </tr>
@@ -60,12 +65,16 @@ export default class DataGridOwn extends Component {
                 <h1>{label}</h1>
                 <table>
                     {
-                        //TODO: CONTACTS.shift();
-                        CONTACTS.map(function(el) {
+                        DATA.map(function(el) {
                             if(!el[0]) {
                                 return <TRTH key={el} td={el} />
                             } else {
-                                return <TRTD key={el} td={el} />
+                                //if (parseInt(el[0]) < max_rows) {
+
+                                    return <TRTD key={el} td={el} />
+                                //} else {
+                                //    return
+                                //}
                             }
 
                         })
@@ -91,7 +100,22 @@ DataGridOwn.propTypes = {
             ['2','Luke Skywalker', '+250976654433','img/luke.gif'],
             ['3','Chewbacca', '+250456784935','img/chewbacca.gif']
         ]
+
+     example on how to read a CSV file in python and convert it like so:
+
+        # Read the CSV file
+        csv_data = []
+        with open('some_csv_file.csv', 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in reader:
+                csv_data.append(row)
      */
-    'csv_data': PropTypes.object
+    'csv_data': PropTypes.object,
+
+    /**
+     * Number of maximum column rows to render. Default is 10
+     */
+        // TODO: make this work.
+    'max_rows': PropTypes.number || 10
 
 };
